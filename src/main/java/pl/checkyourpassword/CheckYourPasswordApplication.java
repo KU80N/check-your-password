c0f98886f24ad33522770b051e1b3e1a1000e4b6
KU80N <jmazurek.contact@gmail.com>
2025-02-04 11:55:40 +0100

    Initial commit

---
+++ src/main/java/pl/checkyourpassword/CheckYourPasswordApplication.java
@@ -0,0 +1,13 @@
+package pl.checkyourpassword;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CheckYourPasswordApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CheckYourPasswordApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/checkyourpassword/CheckYourPasswordApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.checkyourpassword;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CheckYourPasswordApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
